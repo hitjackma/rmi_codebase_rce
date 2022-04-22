ecaba747f4e031ee494c610f1231decbaafa38f1
hitjackma <hitjackma@163.com>
2022-04-22 18:35:45 +0800

    Server's code

---
+++ rmi_rce_codebase/Calc.java
@@ -0,0 +1,18 @@
+//package rmi_rce_codebase;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Calc extends UnicastRemoteObject implements ICalc {
+    public Calc() throws RemoteException {}
+
+    public Integer sum(List<Integer> params) throws RemoteException {
+        Integer sum = 0;
+        for (Integer param: params) {
+            sum += param;
+        }
+        return sum;
+    }
+}
---
+++ rmi_rce_codebase/ICalc.java
@@ -0,0 +1,8 @@
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface ICalc extends Remote {
+    public Integer sum(List<Integer> params) throws RemoteException;
+}
